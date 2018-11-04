1296a508a906f722a6707237ae7b302ab108a48d
Noah Yoshida <nyoshida@nd.edu>
2018-11-03 23:34:24 -0400

    enter key doesnt work

--- src/app.py
+++ src/app.py
@@ -18,9 +18,9 @@ def index():
         query_string = request.query_string.decode("utf-8")
         chat_key = request.args.get("ChatKey") 
         video_id = request.args.get("VideoID")
-        new_url = base_url + "{}*{}".format(video_id, chat_key)
-        if not chat_key:
+        if len(chat_key.strip()) == 0:
             chat_key = "yt_share_default"
+        new_url = base_url + "{}*{}".format(video_id, chat_key)
         return new_url 
 
     return render_template('index.html') 
@@ -38,6 +38,8 @@ def send_video_page(video_code):
         video_key, chat_key = data
     else:
         video_key = data[0]
+        chat_key = "ytshare_default_chat"
+    if len(chat_key) == 0:
         chat_key = "ytshare_default_chat" 
     print(video_key, chat_key)
     return render_template('video_page.html', 
